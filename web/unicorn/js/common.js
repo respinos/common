634da0438d4ab1aea9bb75319b88a958c1cb3254
Roger Espinosa <roger@umich.edu>
2013-03-07 13:34:14 -0500

    making login.js work for non-babel apps

--- web/unicorn/js/common.js
+++ web/unicorn/js/common.js
@@ -7,9 +7,11 @@
 
     var toload = [
         prefix + 'common/unicorn/vendors/js/modernizr.custom.79639.js',
-        // '//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js',
-        '//code.jquery.com/jquery-1.9.1.min.js',
-        '//code.jquery.com/jquery-migrate-1.1.0.min.js',
+        '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+
+        // '//code.jquery.com/jquery-1.9.1.min.js',
+        // '//code.jquery.com/jquery-migrate-1.1.0.min.js',
+        prefix + 'common/unicorn/vendors/js/jquery-migrate-1.1.0.min.js',
         prefix + 'common/unicorn/vendors/js/underscore-min.js',
         prefix + 'common/unicorn/vendors/fancyBox/jquery.fancybox.js',
         prefix + 'common/unicorn/vendors/bootstrap/js/bootstrap-modal.js',
--- web/unicorn/js/login.js
+++ web/unicorn/js/login.js
@@ -1,20 +1,37 @@
 var HT = HT || {};
 head.ready(function() {
 
-    var WAYF_URL = '//roger-full.babel.hathitrust.org/sandbox/cgi/wayffish';
+    var SCRIPT_URL = $('script[src$="login.js"]').attr('src');
+    var PING_URL; var PING_DOMAIN;
+    if ( SCRIPT_URL.indexOf("//") >= 0 ) {
+        PING_DOMAIN = SCRIPT_URL.split('/')[2];
+        PING_URL = 'https://' + PING_DOMAIN + '/cgi/ping';
+    } else {
+        PING_URL = '/cgi/ping';
+        PING_DOMAIN = window.location.hostname;
+    }
 
     var $button = $("#login-button");
     if ( ! $button.length ) {
         // nothing to see here
+        var $logout_link = $("#logout-link");
+        if ( window.location.href.indexOf("/shcgi/") > -1 ) {
+            $logout_link.click(function(e) {
+                e.preventDefault();
+                bootbox.alert("<p>Please quit your browser to logout.</p>");
+                return false;
+            })
+        }
         return;
     }
 
     HT.login_callback = function(data) {
+
         $button.click(function(e) {
             e.preventDefault();
             $button.addClass("active");
             var $block = $(
-                '<form>' + 
+                '<form method="POST">' + 
                     '<div class="headline">' + 
                         '<p>Log in with your partner institution<br />&mdash; no signup is necessary.</p>' +
                     '</div>' +
@@ -35,15 +52,32 @@ head.ready(function() {
                             '<li><a href="#">What are the benefits of logging in? &raquo;</a></li>' +
                         '</ul>' +
                     '</div>' +
+                    '<input type="hidden" name="target" value="" />' + 
                 '</form>'
             );
 
             var $select = $block.find("select[name=mdp]");
+
+            var target = window.location.href;
+            if ( target.indexOf("babel.hathitrust") < 0 ) {
+                // not a babel app, need to route through ping/pong
+                target = HT.get_pong_target(target);
+            }
+
+            $block.find("input[name=target]").val(encodeURI(target));
     
-            $.each(data, function() {
+            $.each(data.idp_list, function() {
                 var $option = $("<option></option>").appendTo($select);
-                $option.val(this.value);
-                $option.text(this.label);
+                $option.val(this.idp_url);
+                $option.text(this.name);
+                if ( this.selected ) {
+                    $option.attr('selected', 'selected');
+                }
+                if ( this.sdrinst == 'uom' ) {
+                    $option = $("<option></option>").appendTo($select);
+                    $option.val('https://test.babel.hathitrust.org/Shibboleth.sso/' + this.sdrinst + '?target=___TARGET___');
+                    $option.text(this.name + ' DEV');                
+                }
             })
 
             var $dialog = bootbox.dialog(
@@ -56,16 +90,25 @@ head.ready(function() {
                 $button.removeClass("active");
                 $dialog.modal("hide");
             })
-            $dialog.find(".btn-cancel").click(function() {
-                $button.removeClass("active");
-                $dialog.modal("hide");
+
+            $dialog.find("button.continue").click(function(e) {
+                e.preventDefault();
+                var href = $select.val();
+                if ( href == '___TARGET___' ) {
+                    href = target.replace("http:", "https:");
+                } else {
+                    target = target.replace('/cgi/', '/shcgi/').replace('http://', 'https://');
+                    href = href.replace('___TARGET___', encodeURI(target));
+                }
+                window.location.href = href;
             })
+
         })                
     }
 
     $.ajax({
         type : "GET",
-        url : WAYF_URL,
+        url : PING_URL,
         async : true,
         jsonp : 'callback',
         dataType : 'jsonp',
