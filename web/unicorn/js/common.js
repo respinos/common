04a27cff4f54a03dea5ef904773f678d07895a3c
Roger Espinosa <roger@umich.edu>
2013-03-28 08:58:07 -0400

    mv vendors -> vendor

--- web/unicorn/js/common.js
+++ web/unicorn/js/common.js
@@ -5,21 +5,21 @@
         prefix = '//test.babel.hathitrust.org/';
     }
 
-    head.js(prefix + 'common/unicorn/vendors/js/modernizr.custom.77754.js');
+    head.js(prefix + 'common/unicorn/vendor/js/modernizr.custom.77754.js');
 
     var toload = [
         '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
 
         // '//code.jquery.com/jquery-1.9.1.min.js',
         // '//code.jquery.com/jquery-migrate-1.1.0.min.js',
-        prefix + 'common/unicorn/vendors/js/jquery-migrate-1.1.0.min.js',
-        prefix + 'common/unicorn/vendors/js/underscore-min.js',
-        prefix + 'common/unicorn/vendors/fancyBox/jquery.fancybox.js',
-        prefix + 'common/unicorn/vendors/bootstrap/js/bootstrap-modal.js',
-        prefix + 'common/unicorn/vendors/bootstrap/js/bootstrap-button.js',
-        prefix + 'common/unicorn/vendors/bootstrap/js/bootstrap-dropdown.js',
-        prefix + 'common/unicorn/vendors/bootstrap/js/bootstrap-tooltip.js',
-        prefix + 'common/unicorn/vendors/bootstrap/js/bootbox.min.js',
+        prefix + 'common/unicorn/vendor/js/jquery-migrate-1.1.0.min.js',
+        prefix + 'common/unicorn/vendor/js/underscore-min.js',
+        prefix + 'common/unicorn/vendor/fancyBox/jquery.fancybox.js',
+        prefix + 'common/unicorn/vendor/bootstrap/js/bootstrap-modal.js',
+        prefix + 'common/unicorn/vendor/bootstrap/js/bootstrap-button.js',
+        prefix + 'common/unicorn/vendor/bootstrap/js/bootstrap-dropdown.js',
+        prefix + 'common/unicorn/vendor/bootstrap/js/bootstrap-tooltip.js',
+        prefix + 'common/unicorn/vendor/bootstrap/js/bootbox.min.js',
         prefix + 'common/unicorn/js/feedback.js',
         prefix + 'common/unicorn/js/login.js',
     ]
