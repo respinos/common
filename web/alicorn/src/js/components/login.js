853ccdc7a159ff01a70cb8a4151468f205d56904
Roger Espinosa <roger@umich.edu>
2021-06-28 15:04:39 -0400

    install AUP URL

--- web/alicorn/src/js/components/login.js
+++ web/alicorn/src/js/components/login.js
@@ -47,7 +47,7 @@ head.ready(function() {
                         '</div>' +
                         '<input type="hidden" name="target" value="" />' + 
                     '</form>' + 
-                    '<p class="aup-notice">By logging into HathiTrust, you agree to follow our <a href="#">Acceptable Use Policy</a>.</p>' + 
+                    '<p class="aup-notice">By logging into HathiTrust, you agree to follow our <a href="https://www.hathitrust.org/acceptable-use">Acceptable Use Policy</a>.</p>' + 
                 '</div>' + 
                 '<div class="modal__footer">' + 
                     '<div class="questions">' + 
