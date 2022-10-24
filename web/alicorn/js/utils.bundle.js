e86917d096da86904b765ea5ef6c8f31faa1480e
Caryl Wyatt <caryl.e.wyatt@gmail.com>
2022-10-24 11:43:58 -0500

    add roger's changes to enable local testing

--- web/alicorn/js/utils.bundle.js
+++ web/alicorn/js/utils.bundle.js
@@ -22049,13 +22049,15 @@ head.ready(function () {
     var $link = $(this);
     var href = $link.attr("href");
 
-    if (!href) {// no op
+    if (!href) {
+      // no op
+      return;
     } else if (href.indexOf("www.hathitrust.org") > -1) {
-      $link.attr("href", href.replace(/[\w-\.]*www.hathitrust.org/, HT.www_domain));
+      href = href.replace(/[\w-\.]*www.hathitrust.org/, HT.www_domain);
     } else if (href.indexOf("babel.hathitrust.org") > -1) {
-      $link.attr("href", href.replace(/[\w-\.]*babel.hathitrust.org/, HT.service_domain));
+      href = href.replace(/[\w-\.]*babel.hathitrust.org/, HT.service_domain);
     } else if (href.indexOf("catalog.hathitrust.org") > -1) {
-      $link.attr("href", href.replace(/[\w-\.]*catalog.hathitrust.org/, HT.catalog_domain));
+      href = href.replace(/[\w-\.]*catalog.hathitrust.org/, HT.catalog_domain);
     } else if (href.indexOf("/hdl.handle.net") > -1) {
       // e.g. https://hdl.handle.net/2027/uc2.ark:/13960/t9s19288f
       // var tmp = href.split("/");
@@ -22068,32 +22070,38 @@ head.ready(function () {
         var tmp2 = tmp[1].split('=');
         href += ";seq=" + tmp2[2];
       }
-
-      $link.attr("href", href);
     }
+
+    $link.attr("href", href.replace('https:', location.protocol));
   });
   $("form[action]").each(function () {
     var $form = $(this);
     var action = $form.attr("action");
 
-    if (!action) {// no op
+    if (!action) {
+      // no op
+      return;
     } else if (action.indexOf('babel.hathitrust.org') > -1) {
-      $form.attr("action", action.replace(/[\w-\.]*babel.hathitrust.org/, HT.service_domain));
+      action = action.replace(/[\w-\.]*babel.hathitrust.org/, HT.service_domain);
     } else if (action.indexOf("catalog.hathitrust.org") > -1) {
-      $form.attr("action", action.replace(/[\w-\.]*catalog.hathitrust.org/, HT.catalog_domain));
+      action = action.replace(/[\w-\.]*catalog.hathitrust.org/, HT.catalog_domain);
     }
+
+    $form.attr("action", action.replace('https:', location.protocol));
   });
   $("[data-href]").each(function () {
     var $link = $(this);
     var href = $link.data('href');
 
-    if (!href) {// no op
+    if (!href) {
+      // no op
+      return;
     } else if (href.indexOf("www.hathitrust.org") > -1) {
-      $link.data("href", href.replace(/[\w-\.]*www.hathitrust.org/, HT.www_domain));
+      href = href.replace(/[\w-\.]*www.hathitrust.org/, HT.www_domain);
     } else if (href.indexOf("babel.hathitrust.org") > -1) {
-      $link.data("href", href.replace(/[\w-\.]*babel.hathitrust.org/, HT.service_domain));
+      href = href.replace(/[\w-\.]*babel.hathitrust.org/, HT.service_domain);
     } else if (href.indexOf("catalog.hathitrust.org") > -1) {
-      $link.data("href", href.replace(/[\w-\.]*catalog.hathitrust.org/, HT.catalog_domain));
+      href = href.replace(/[\w-\.]*catalog.hathitrust.org/, HT.catalog_domain);
     } else if (href.indexOf("/hdl.handle.net") > -1) {
       // e.g. https://hdl.handle.net/2027/uc2.ark:/13960/t9s19288f
       // var tmp = href.split("/");
@@ -22105,27 +22113,30 @@ head.ready(function () {
         var tmp2 = tmp[1].split('=');
         href += ";seq=" + tmp2[2];
       }
-
-      $link.data("href", href);
     }
+
+    $link.data('href', href.replace('https:', location.protocol));
   });
   $("[src]").each(function () {
     var $link = $(this);
     var href = $link.attr('src');
 
-    if (!href) {// no op
+    if (!href) {
+      // no op
+      return;
     } else if (href.indexOf("www.hathitrust.org") > -1) {// $link.attr("src", href.replace(/[\w-\.]*www.hathitrust.org/, HT.www_domain));
     } else if (href.indexOf("babel.hathitrust.org") > -1) {
-      $link.attr("src", href.replace(/[\w-\.]*babel.hathitrust.org/, HT.service_domain));
+      href = href.replace(/[\w-\.]*babel.hathitrust.org/, HT.service_domain);
     } else if (href.indexOf("catalog.hathitrust.org") > -1) {
-      $link.data("src", href.replace(/[\w-\.]*catalog.hathitrust.org/, HT.catalog_domain));
+      href = href.replace(/[\w-\.]*catalog.hathitrust.org/, HT.catalog_domain);
     } else if (href.indexOf("/hdl.handle.net") > -1) {
       // e.g. https://hdl.handle.net/2027/uc2.ark:/13960/t9s19288f
       // var tmp = href.split("/");
       // href = "http:/" + babel_string + "/cgi/pt?id=" + tmp.pop();
       href = href.replace("//hdl.handle.net/2027/", "//" + HT.service_domain + "/cgi/pt?id=");
-      $link.attr("src", href);
     }
+
+    $link.attr("src", href.replace('https:', location.protocol));
   });
 });
 "use strict";
--- web/alicorn/src/js/components/staging.js
+++ web/alicorn/src/js/components/staging.js
@@ -7,12 +7,13 @@ head.ready(function() {
       var href = $link.attr("href");
       if ( ! href ) {
           // no op
+          return;
       } else if ( href.indexOf("www.hathitrust.org") > -1 ) {
-          $link.attr("href", href.replace(/[\w-\.]*www.hathitrust.org/, HT.www_domain));
+          href = href.replace(/[\w-\.]*www.hathitrust.org/, HT.www_domain);
       } else if ( href.indexOf("babel.hathitrust.org") > -1 ) {
-          $link.attr("href", href.replace(/[\w-\.]*babel.hathitrust.org/, HT.service_domain));
+          href = href.replace(/[\w-\.]*babel.hathitrust.org/, HT.service_domain);
       } else if ( href.indexOf("catalog.hathitrust.org") > -1 ) {
-          $link.attr("href", href.replace(/[\w-\.]*catalog.hathitrust.org/, HT.catalog_domain));
+          href = href.replace(/[\w-\.]*catalog.hathitrust.org/, HT.catalog_domain);
       } else if ( href.indexOf("/hdl.handle.net") > -1 ) {
           // e.g. https://hdl.handle.net/2027/uc2.ark:/13960/t9s19288f
           // var tmp = href.split("/");
@@ -24,8 +25,8 @@ head.ready(function() {
             var tmp2 = tmp[1].split('=');
             href += ";seq=" + tmp2[2];
           }
-          $link.attr("href", href);
       }
+      $link.attr("href", href.replace('https:', location.protocol));
   })
 
   $("form[action]").each(function() {
@@ -33,11 +34,13 @@ head.ready(function() {
     var action = $form.attr("action");
     if ( ! action ) {
       // no op
+      return;
     } else if ( action.indexOf('babel.hathitrust.org') > -1 ) {
-      $form.attr("action", action.replace(/[\w-\.]*babel.hathitrust.org/, HT.service_domain));
+      action = action.replace(/[\w-\.]*babel.hathitrust.org/, HT.service_domain);
     } else if ( action.indexOf("catalog.hathitrust.org") > -1 ) {
-      $form.attr("action", action.replace(/[\w-\.]*catalog.hathitrust.org/, HT.catalog_domain));
+      action = action.replace(/[\w-\.]*catalog.hathitrust.org/, HT.catalog_domain);
     }
+    $form.attr("action", action.replace('https:', location.protocol));
   })
 
   $("[data-href]").each(function() {
@@ -45,12 +48,13 @@ head.ready(function() {
     var href = $link.data('href');
     if ( ! href ) {
         // no op
+        return;
     } else if ( href.indexOf("www.hathitrust.org") > -1 ) {
-        $link.data("href", href.replace(/[\w-\.]*www.hathitrust.org/, HT.www_domain));
+        href = href.replace(/[\w-\.]*www.hathitrust.org/, HT.www_domain);
     } else if ( href.indexOf("babel.hathitrust.org") > -1 ) {
-        $link.data("href", href.replace(/[\w-\.]*babel.hathitrust.org/, HT.service_domain));
+        href = href.replace(/[\w-\.]*babel.hathitrust.org/, HT.service_domain);
     } else if ( href.indexOf("catalog.hathitrust.org") > -1 ) {
-        $link.data("href", href.replace(/[\w-\.]*catalog.hathitrust.org/, HT.catalog_domain));
+        href = href.replace(/[\w-\.]*catalog.hathitrust.org/, HT.catalog_domain);
     } else if ( href.indexOf("/hdl.handle.net") > -1 ) {
         // e.g. https://hdl.handle.net/2027/uc2.ark:/13960/t9s19288f
         // var tmp = href.split("/");
@@ -61,8 +65,8 @@ head.ready(function() {
           var tmp2 = tmp[1].split('=');
           href += ";seq=" + tmp2[2];
         }
-        $link.data("href", href);
     }
+    $link.data('href', href.replace('https:', location.protocol));
   })
 
   $("[src]").each(function() {
@@ -70,19 +74,20 @@ head.ready(function() {
     var href = $link.attr('src');
     if ( ! href ) {
         // no op
+        return;
     } else if ( href.indexOf("www.hathitrust.org") > -1 ) {
         // $link.attr("src", href.replace(/[\w-\.]*www.hathitrust.org/, HT.www_domain));
     } else if ( href.indexOf("babel.hathitrust.org") > -1 ) {
-        $link.attr("src", href.replace(/[\w-\.]*babel.hathitrust.org/, HT.service_domain));
+        href = href.replace(/[\w-\.]*babel.hathitrust.org/, HT.service_domain);
     } else if ( href.indexOf("catalog.hathitrust.org") > -1 ) {
-        $link.data("src", href.replace(/[\w-\.]*catalog.hathitrust.org/, HT.catalog_domain));
+        href = href.replace(/[\w-\.]*catalog.hathitrust.org/, HT.catalog_domain);
     } else if ( href.indexOf("/hdl.handle.net") > -1 ) {
         // e.g. https://hdl.handle.net/2027/uc2.ark:/13960/t9s19288f
         // var tmp = href.split("/");
         // href = "http:/" + babel_string + "/cgi/pt?id=" + tmp.pop();
         href = href.replace("//hdl.handle.net/2027/", "//" + HT.service_domain + "/cgi/pt?id=");
-        $link.attr("src", href);
     }
+    $link.attr("src", href.replace('https:', location.protocol));
   })
 
 
