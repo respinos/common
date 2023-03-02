a232a887139ef06a924e8799bd1ef22abe9975af
Caryl Wyatt <caryl.e.wyatt@gmail.com>
2023-03-02 12:30:30 -0600

    add navbar items

--- src/js/App.svelte
+++ src/js/App.svelte
@@ -1,8 +1,10 @@
 
 <script>
-    import Button from "./Button.svelte";
+    import Navbar from "./components/Navbar.svelte"
+    import Button from "./components/Button.svelte";
 </script>
 
 <div>
+    <Navbar />
     <Button />
 </div>
---
+++ src/js/components/Navbar.svelte
@@ -0,0 +1,60 @@
+<script>
+import { Collapse, Dropdown, Popover } from 'bootstrap'
+</script>
+
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            About
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Welcome to HathiTrust</a></li>
+            <li><a class="dropdown-item" href="#">Our Mission &amp; History</a></li>
+            <li><a class="dropdown-item" href="#">HathiTrust Research Center (HTRC)</a></li>
+            <li><a class="dropdown-item" href="#">Our Team</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           The Collection 
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">About the Collection</a></li>
+            <li><a class="dropdown-item" href="#">How to Search &amp; Access</a></li>
+            <li><a class="dropdown-item" href="#">Preservation</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Member Libraries
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Member Library Overview</a></li>
+            <li><a class="dropdown-item" href="#">Member List</a></li>
+            <li><a class="dropdown-item" href="#">Services & Programs</a></li>
+            <li><a class="dropdown-item" href="#">Resources for Librarians</a></li>
+          </ul>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link active" href="#">Join</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">News &amp; Events</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Newsletter</a></li>
+            <li><a class="dropdown-item" href="#">Blog</a></li>
+            <li><a class="dropdown-item" href="#">Events &amp; Webinars</a></li>
+            <li><a class="dropdown-item" href="#">Press</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
