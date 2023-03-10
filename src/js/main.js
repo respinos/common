6df0cc0d43e1b62c77c2646c7dee1202e3675019
Roger Espinosa <roger@umich.edu>
2023-03-10 10:32:14 -0500

    hacking the login/wayf modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 // Import our custom CSS
 import '../scss/styles.scss'
+import { setupHTEnv } from './lib/utils';
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+setupHTEnv();
+
---
+++ src/js/wayf_test.js
@@ -0,0 +1,7 @@
+import WayfForm from "./WayfForm.svelte";
+
+const app = new WayfForm({
+  target: document.getElementById("login"),
+});
+
+export default app;
