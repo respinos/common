2a469556172179d29b6d48811606d6dd11eff6ab
Roger Espinosa <roger@umich.edu>
2023-03-24 17:42:02 -0400

    aliases main.css and main.js take 3

---
+++ build-redirects.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import { chdir } from 'node:process';
+import { readdirSync, writeFileSync } from 'node:fs';
+import path from 'node:path';
+
+const redirects = [];
+chdir('dist/assets');
+let filenames = readdirSync('.');
+filenames.forEach((filename) => {
+  let match;
+  if ( match = filename.match(/(\w+)-[^\.]+\.(js|css)/) ) {
+    let ext = match[2];
+    let basename = match[1];
+    redirects.push(`/assets/${basename}.${ext} /assets/${filename} 200`);
+  }
+})
+
+writeFileSync('../_redirects', redirects.join("\n"))
+console.log("-- created _redirects")
