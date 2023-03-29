df9df9a7e81b106ce7fa6ad3226deb4c30a88f23
Caryl Wyatt <carylw@hathitrust.org>
2023-03-29 15:39:20 -0500

    add basic bootstrap HTML and stories

---
+++ src/js/components/SearchBar/index.svelte
@@ -0,0 +1,48 @@
+<script>
+</script>
+
+<div>
+  <form>
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="basic-addon1"
+        ><i class="fa-solid fa-magnifying-glass fa-fw" /></span
+      >
+      <input
+        type="text"
+        class="form-control"
+        aria-label="Text input with dropdown button"
+        placeholder="Search using keywords"
+      />
+
+      <button
+        class="btn btn-outline-secondary dropdown-toggle"
+        type="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false">Collection</button
+      >
+      <ul class="dropdown-menu dropdown-menu-end">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+        <li><hr class="dropdown-divider" /></li>
+        <li><a class="dropdown-item" href="#">Separated link</a></li>
+      </ul>
+      <button
+        class="btn btn-outline-secondary dropdown-toggle"
+        type="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false">All Fields</button
+      >
+      <ul class="dropdown-menu dropdown-menu-end">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+        <li><hr class="dropdown-divider" /></li>
+        <li><a class="dropdown-item" href="#">Separated link</a></li>
+      </ul>
+      <button class="btn btn-outline-secondary" type="button" id="button-addon2"
+        >Search</button
+      >
+    </div>
+  </form>
+</div>
