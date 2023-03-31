700fbd6995120ff607bb6a15fe51a5ad5d321495
Caryl Wyatt <carylw@hathitrust.org>
2023-03-31 12:52:24 -0500

    add search toggle functions

--- src/js/components/Header/index.svelte
+++ src/js/components/Header/index.svelte
@@ -1,6 +1,21 @@
 <script>
   import Navbar from '../Navbar/';
   import SearchBar from '../SearchBar';
+
+  export let search_state = 'default';
+  let searchFormDisplayed = search_state == 'default';
+
+  function displaySearchForm() {
+    if (search_state == 'default') {
+      return true;
+    } else if (search_state == 'toggle') {
+      return searchFormDisplayed;
+    }
+    return false;
+  }
+  function toggleSearch() {
+    searchFormDisplayed = !searchFormDisplayed;
+  }
 </script>
 
 <div>
