a3c311d4fb0475362294995594258e6df4463635
Caryl Wyatt <carylw@hathitrust.org>
2023-04-07 16:05:56 -0500

    update select dropdown caret

--- src/js/components/SearchBar/index.svelte
+++ src/js/components/SearchBar/index.svelte
@@ -83,7 +83,7 @@
           >
         </div>
         <select
-          class="form-select"
+          class="form-select select-caret"
           aria-label="Default select example"
           bind:this={_select}
           on:change={_updateSelect}
@@ -93,7 +93,7 @@
         </select>
         {#if index == 'library'}
           <select
-            class="form-select"
+            class="form-select select-caret"
             aria-label="Default select example"
             bind:this={_searchtype}
             on:change={_updateSearchType}
@@ -227,6 +227,15 @@
         color: var(--color-primary-500);
       }
     }
+  }
+  select.select-caret {
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 320 512'%3E%3Cpath d='M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z'/%3E%3C/svg%3E");
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    -webkit-border-radius: 0px;
+    border: none;
+    appearance: none;
+    outline-width: 0;
   }
   @media (min-width: 768px) {
     #searchbar-form {
