b4fe5f02b3998c9351877108e325b99ac2cf606e
Caryl Wyatt <carylw@hathitrust.org>
2023-04-18 15:13:08 -0500

    added basic tests to nav

--- src/js/components/Navbar/Navbar.stories.js
+++ src/js/components/Navbar/Navbar.stories.js
@@ -14,7 +14,24 @@ export const Default = {
       defaultViewport: 'bsXl',
     },
   },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(await canvas.getByTitle('HathiTrust logo')).toBeInTheDocument()
+    expect(await canvas.getByRole('navigation')) 
+    
+    const mainMenu = await canvas.getByRole('navigation'); 
+    // await userEvent.click(mobileMenuButton);
+  },
 };
+export const DesktopDropdownMenuSelected = {
+  parameters: { ...Default.parameters },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    
+    const mainMenu = await canvas.getByText(/member libraries/i); 
+    await userEvent.click(mainMenu);
+  }, 
+}
 export const DesktopLoginModalOpen = {
   parameters: { ...Default.parameters },
   args: {
@@ -51,6 +68,18 @@ export const MobileOpenMenu = {
     await userEvent.click(mobileMenuButton);
   },
 };
+export const MobileDropdownMenuSelected = {
+ parameters: { ...Mobile.parameters },
+ play: async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  const mobileMenuButton = await canvas.getByRole('button', {name: 'Toggle navigation'}); 
+  const mainMenu = await canvas.getByText(/member libraries/i); 
+
+  await userEvent.click(mobileMenuButton); 
+  await userEvent.click(mainMenu); 
+ }
+};
 export const MobileLoggedIn = {
   parameters: { ...Mobile.parameters },
   args: {
@@ -64,3 +93,17 @@ export const MobileLoggedInWithNotifications = {
     hasNotification: true,
   },
 };
+export const MobileLoggedInMyAccountDropdown = {
+  parameters: { ...Mobile.parameters },
+  args: { ...MobileLoggedInWithNotifications.args },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+  
+    const mobileMenuButton = await canvas.getByRole('button', {name: 'Toggle navigation'}); 
+    const myAccount = await canvas.getByText(/my account/i); 
+  
+    await userEvent.click(mobileMenuButton); 
+    await userEvent.click(myAccount); 
+   }
+
+}
