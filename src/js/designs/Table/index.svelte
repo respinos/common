c92a6993d46e915f5dafe8391bc18c9d4f8bf0c6
Caryl Wyatt <carylw@hathitrust.org>
2023-05-09 16:25:01 -0500

    add table design and story

---
+++ src/js/designs/Table/index.svelte
@@ -0,0 +1,420 @@
+<div class="apps">
+  <table class="btable">
+    <thead>
+      <tr>
+        <th>Item Link</th>
+        <th>Original Source</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/chi.096470015"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">c.1 v.2 1874/75</span></a
+          >
+        </td>
+        <td> University of Chicago </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/chi.096470031"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">c.1 1885</span></a
+          >
+        </td>
+        <td> University of Chicago </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/chi.096470057"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">c.1 1887</span></a
+          >
+        </td>
+        <td> University of Chicago </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/chi.096470081"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">c.1 1891</span></a
+          >
+        </td>
+        <td> University of Chicago </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/chi.096470112"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">c.1 1895</span></a
+          >
+        </td>
+        <td> University of Chicago </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/chi.096470138"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">c.1 1904 pt.1</span></a
+          >
+        </td>
+        <td> University of Chicago </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/chi.096470146"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">c.1 1904 pt.2</span></a
+          >
+        </td>
+        <td> University of Chicago </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/coo.31924007104676"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1874</span></a
+          >
+        </td>
+        <td> Cornell University </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/coo.31924093038762"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1875</span></a
+          >
+        </td>
+        <td> Cornell University </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/umn.31951002274876s"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1875</span></a
+          >
+        </td>
+        <td> University of Minnesota </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/umn.31951002274877q"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1876</span></a
+          >
+        </td>
+        <td> University of Minnesota </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/umn.31951002274878o"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1877</span></a
+          >
+        </td>
+        <td> University of Minnesota </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/umn.31951002274879m"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1878</span></a
+          >
+        </td>
+        <td> University of Minnesota </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/umn.319510022748801"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1879</span></a
+          >
+        </td>
+        <td> University of Minnesota </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/umn.31951002274881z"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1880</span></a
+          >
+        </td>
+        <td> University of Minnesota </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/umn.31951002274882x"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1881</span></a
+          >
+        </td>
+        <td> University of Minnesota </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/umn.31951002274884t"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1882/84</span></a
+          >
+        </td>
+        <td> University of Minnesota </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/umn.31951002274883v"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1882</span></a
+          >
+        </td>
+        <td> University of Minnesota </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/mdp.35112103801421"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1883/1884</span></a
+          >
+        </td>
+        <td> University of Michigan </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/coo.31924093038846"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1884/86</span></a
+          >
+        </td>
+        <td> Cornell University </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/mdp.35112103801413"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1885/1886</span></a
+          >
+        </td>
+        <td> University of Michigan </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/coo.31924093038853"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1886/88</span></a
+          >
+        </td>
+        <td> Cornell University </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/mdp.35112103801405"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1887/1888</span></a
+          >
+        </td>
+        <td> University of Michigan </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/mdp.35112103801991"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1888/1890</span></a
+          >
+        </td>
+        <td> University of Michigan </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/umn.31951002274887n"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1888/90</span></a
+          >
+        </td>
+        <td> University of Minnesota </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/coo.31924093038861"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1888/90</span></a
+          >
+        </td>
+        <td> Cornell University </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/mdp.35112103801983"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1891/1892</span></a
+          >
+        </td>
+        <td> University of Michigan </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/coo.31924093038879"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1892/94</span></a
+          >
+        </td>
+        <td> Cornell University </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/umn.31951d001462701"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1892/98</span></a
+          >
+        </td>
+        <td> University of Minnesota </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/umn.31951002274890y"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1898/1900</span></a
+          >
+        </td>
+        <td> University of Minnesota </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/mdp.35112103801975"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1901/1902</span></a
+          >
+        </td>
+        <td> University of Michigan </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/coo.31924093038887"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1902/04</span></a
+          >
+        </td>
+        <td> Cornell University </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/mdp.35112103801967"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1903/1904</span></a
+          >
+        </td>
+        <td> University of Michigan </td>
+      </tr>
+      <tr>
+        <td>
+          <a
+            href="https://hdl.handle.net/2027/umn.31951002274893s"
+            class="rights-Array fulltext"
+            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
+            view
+            <span class="IndItem">1904/06</span></a
+          >
+        </td>
+        <td> University of Minnesota </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
