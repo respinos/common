68f1b0d7ceb2485d36d882c7bc01836ad460729e
Caryl Wyatt <carylw@hathitrust.org>
2023-05-10 10:49:44 -0500

    table styles

--- src/js/designs/Table/index.svelte
+++ src/js/designs/Table/index.svelte
@@ -1,5 +1,5 @@
 <div class="apps p-3 mb-3">
-  <table class="btable">
+  <table class="table-branded">
     <thead>
       <tr>
         <th>Item Link</th>
@@ -12,9 +12,9 @@
           <a
             href="https://hdl.handle.net/2027/chi.096470015"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">c.1 v.2 1874/75</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">c.1 v.2 1874/75</span></a
           >
         </td>
         <td> University of Chicago </td>
@@ -24,9 +24,9 @@
           <a
             href="https://hdl.handle.net/2027/chi.096470031"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">c.1 1885</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">c.1 1885</span></a
           >
         </td>
         <td> University of Chicago </td>
@@ -36,9 +36,9 @@
           <a
             href="https://hdl.handle.net/2027/chi.096470057"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">c.1 1887</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">c.1 1887</span></a
           >
         </td>
         <td> University of Chicago </td>
@@ -48,9 +48,9 @@
           <a
             href="https://hdl.handle.net/2027/chi.096470081"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">c.1 1891</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">c.1 1891</span></a
           >
         </td>
         <td> University of Chicago </td>
@@ -60,9 +60,9 @@
           <a
             href="https://hdl.handle.net/2027/chi.096470112"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">c.1 1895</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">c.1 1895</span></a
           >
         </td>
         <td> University of Chicago </td>
@@ -72,9 +72,9 @@
           <a
             href="https://hdl.handle.net/2027/chi.096470138"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">c.1 1904 pt.1</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">c.1 1904 pt.1</span></a
           >
         </td>
         <td> University of Chicago </td>
@@ -84,9 +84,9 @@
           <a
             href="https://hdl.handle.net/2027/chi.096470146"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">c.1 1904 pt.2</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">c.1 1904 pt.2</span></a
           >
         </td>
         <td> University of Chicago </td>
@@ -96,9 +96,9 @@
           <a
             href="https://hdl.handle.net/2027/coo.31924007104676"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1874</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1874</span></a
           >
         </td>
         <td> Cornell University </td>
@@ -108,9 +108,9 @@
           <a
             href="https://hdl.handle.net/2027/coo.31924093038762"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1875</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1875</span></a
           >
         </td>
         <td> Cornell University </td>
@@ -120,9 +120,9 @@
           <a
             href="https://hdl.handle.net/2027/umn.31951002274876s"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1875</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1875</span></a
           >
         </td>
         <td> University of Minnesota </td>
@@ -132,9 +132,9 @@
           <a
             href="https://hdl.handle.net/2027/umn.31951002274877q"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1876</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1876</span></a
           >
         </td>
         <td> University of Minnesota </td>
@@ -144,9 +144,9 @@
           <a
             href="https://hdl.handle.net/2027/umn.31951002274878o"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1877</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1877</span></a
           >
         </td>
         <td> University of Minnesota </td>
@@ -156,9 +156,9 @@
           <a
             href="https://hdl.handle.net/2027/umn.31951002274879m"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1878</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1878</span></a
           >
         </td>
         <td> University of Minnesota </td>
@@ -168,9 +168,9 @@
           <a
             href="https://hdl.handle.net/2027/umn.319510022748801"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1879</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1879</span></a
           >
         </td>
         <td> University of Minnesota </td>
@@ -180,9 +180,9 @@
           <a
             href="https://hdl.handle.net/2027/umn.31951002274881z"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1880</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1880</span></a
           >
         </td>
         <td> University of Minnesota </td>
@@ -192,9 +192,9 @@
           <a
             href="https://hdl.handle.net/2027/umn.31951002274882x"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1881</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1881</span></a
           >
         </td>
         <td> University of Minnesota </td>
@@ -204,9 +204,9 @@
           <a
             href="https://hdl.handle.net/2027/umn.31951002274884t"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1882/84</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1882/84</span></a
           >
         </td>
         <td> University of Minnesota </td>
@@ -216,9 +216,9 @@
           <a
             href="https://hdl.handle.net/2027/umn.31951002274883v"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1882</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1882</span></a
           >
         </td>
         <td> University of Minnesota </td>
@@ -228,9 +228,9 @@
           <a
             href="https://hdl.handle.net/2027/mdp.35112103801421"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1883/1884</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1883/1884</span></a
           >
         </td>
         <td> University of Michigan </td>
@@ -240,9 +240,9 @@
           <a
             href="https://hdl.handle.net/2027/coo.31924093038846"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1884/86</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1884/86</span></a
           >
         </td>
         <td> Cornell University </td>
@@ -252,9 +252,9 @@
           <a
             href="https://hdl.handle.net/2027/mdp.35112103801413"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1885/1886</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1885/1886</span></a
           >
         </td>
         <td> University of Michigan </td>
@@ -264,9 +264,9 @@
           <a
             href="https://hdl.handle.net/2027/coo.31924093038853"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1886/88</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1886/88</span></a
           >
         </td>
         <td> Cornell University </td>
@@ -276,9 +276,9 @@
           <a
             href="https://hdl.handle.net/2027/mdp.35112103801405"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1887/1888</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1887/1888</span></a
           >
         </td>
         <td> University of Michigan </td>
@@ -288,9 +288,9 @@
           <a
             href="https://hdl.handle.net/2027/mdp.35112103801991"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1888/1890</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1888/1890</span></a
           >
         </td>
         <td> University of Michigan </td>
@@ -300,9 +300,9 @@
           <a
             href="https://hdl.handle.net/2027/umn.31951002274887n"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1888/90</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1888/90</span></a
           >
         </td>
         <td> University of Minnesota </td>
@@ -312,9 +312,9 @@
           <a
             href="https://hdl.handle.net/2027/coo.31924093038861"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1888/90</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1888/90</span></a
           >
         </td>
         <td> Cornell University </td>
@@ -324,9 +324,9 @@
           <a
             href="https://hdl.handle.net/2027/mdp.35112103801983"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1891/1892</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1891/1892</span></a
           >
         </td>
         <td> University of Michigan </td>
@@ -336,9 +336,9 @@
           <a
             href="https://hdl.handle.net/2027/coo.31924093038879"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1892/94</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1892/94</span></a
           >
         </td>
         <td> Cornell University </td>
@@ -348,9 +348,9 @@
           <a
             href="https://hdl.handle.net/2027/umn.31951d001462701"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1892/98</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1892/98</span></a
           >
         </td>
         <td> University of Minnesota </td>
@@ -360,9 +360,9 @@
           <a
             href="https://hdl.handle.net/2027/umn.31951002274890y"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1898/1900</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1898/1900</span></a
           >
         </td>
         <td> University of Minnesota </td>
@@ -372,9 +372,9 @@
           <a
             href="https://hdl.handle.net/2027/mdp.35112103801975"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1901/1902</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1901/1902</span></a
           >
         </td>
         <td> University of Michigan </td>
@@ -384,9 +384,9 @@
           <a
             href="https://hdl.handle.net/2027/coo.31924093038887"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1902/04</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1902/04</span></a
           >
         </td>
         <td> Cornell University </td>
@@ -396,9 +396,9 @@
           <a
             href="https://hdl.handle.net/2027/mdp.35112103801967"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1903/1904</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1903/1904</span></a
           >
         </td>
         <td> University of Michigan </td>
@@ -408,9 +408,9 @@
           <a
             href="https://hdl.handle.net/2027/umn.31951002274893s"
             class="rights-Array fulltext"
-            ><i class="icomoon icomoon-document-2" aria-hidden="true" /> Full
-            view
-            <span class="IndItem">1904/06</span></a
+            ><i class="fa-regular fa-file-lines" aria-hidden="true" />
+            <span>Full view</span>
+            &nbsp; <span class="IndItem">1904/06</span></a
           >
         </td>
         <td> University of Minnesota </td>
