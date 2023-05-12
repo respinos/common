6003c4d16e068e17c05520bcba3e0de02da558f5
Caryl Wyatt <carylw@hathitrust.org>
2023-05-12 15:20:35 -0500

    add feedback form modal and story

---
+++ src/js/components/FeedbackFormModal/FeedbackFormModal.stories.js
@@ -0,0 +1,12 @@
+import FeedbackFormModal from './index.svelte';
+
+export default {
+  title: 'Feedback Form Modal',
+  component: FeedbackFormModal,
+};
+
+export const Default = {
+  args: {
+    isOpen: true,
+  },
+};
---
+++ src/js/components/FeedbackFormModal/index.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { onMount } from 'svelte';
+  import Modal from '../Modal';
+  import FeedbackFormBasic from '../FeedbackFormBasic';
+  let modal;
+  export let isOpen = false;
+
+  export const show = function () {
+    modal.show();
+  };
+
+  export const hide = function () {
+    modal.hide();
+  };
+  onMount(() => {
+    if (isOpen && modal) {
+      modal.show();
+    }
+  });
+
+  $: if (modal && isOpen) {
+    show();
+  }
+  $: if (modal && !isOpen) {
+    hide();
+  }
+</script>
+
+<div>
+  <Modal bind:this={modal} scrollable>
+    <svelte:fragment slot="modal-title">Questions?</svelte:fragment>
+    <svelte:fragment slot="modal-body">
+      <FeedbackFormBasic />
+    </svelte:fragment>
+  </Modal>
+</div>
